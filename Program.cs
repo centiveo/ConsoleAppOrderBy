e8129069aaf100421d8a8c92db05f6117199e1d8
Ebe <ebedeketelaere@gmail.com>
2024-02-20 10:34:19 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleAppOrderBy;
+
+IList<Student> studentList = new List<Student>() 
+{
+                new Student() { StudentID = 1, StudentNaam = "Jan", Leeftijd = 18 } ,
+                new Student() { StudentID = 6, StudentNaam = "Piet", Leeftijd = 18 } ,
+                new Student() { StudentID = 2, StudentNaam = "Joris",  Leeftijd = 15 } ,
+                new Student() { StudentID = 3, StudentNaam = "Dimitri",  Leeftijd = 25 } ,
+                new Student() { StudentID = 4, StudentNaam = "Carmen" , Leeftijd = 20 } ,
+                new Student() { StudentID = 5, StudentNaam = "Pol" , Leeftijd = 19 }
+};
+
+
+//var studentenAflopend = studentList.OrderByDescending(s => s.Leeftijd);
+var studentenOuderDan = studentList.Where(s => s.Leeftijd > 20);
+
+
+foreach (var std in studentenOuderDan)
+    Console.WriteLine(std.StudentNaam + ' ' + std.Leeftijd);
---
+++ Student.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace ConsoleAppOrderBy
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentNaam { get; set; }
+        public int Leeftijd { get; set; }
+
+    }
+}
